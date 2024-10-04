884b353a5ff77e9dc963eccf518755a36d6a0cfe
YuriSherin <YuriSherin@mail.ru>
2024-10-05 04:43:33 +0700

    Change project

--- main.py
+++ main.py
@@ -60,26 +60,6 @@ if __name__ == '__main__':
     p1 = Product('Potato', 50.5, 'Vegetables')
     p2 = Product('Spaghetti', 3.4, 'Groceries')
     p3 = Product('Potato', 5.5, 'Vegetables')
-
     print(p2)  # __str__
-
     s1.add(p1, p2, p3)
-
     print(s1.get_products())
-
-# Вывод на консоль:
-# Первый запуск:
-# Spaghetti, 3.4, Groceries
-# Potato, 50.5, Vegetables
-# Spaghetti, 3.4, Groceries
-# Potato, 5.5, Vegetables
-
-# Второй запуск:
-# Spaghetti, 3.4, Groceries
-# Продукт Potato, 50.5, Vegetables уже есть в магазине
-# Продукт Spaghetti, 3.4, Groceries уже есть в магазине
-# Продукт Potato, 5.5, Vegetables уже есть в магазине
-# Potato, 50.5, Vegetables
-# Spaghetti, 3.4, Groceries
-# Potato, 5.5, Vegetables
-# Как выглядит файл после запусков:
